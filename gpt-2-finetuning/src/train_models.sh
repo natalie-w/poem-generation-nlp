4112bb3168dfc1c712595ce473b335c6ffbdc2d3
Natalie Wang <n.wang@berkeley.edu>
2020-04-13 12:25:07 -0500

    tried different learning rates

--- gpt-2-finetuning/src/train_models.sh
+++ gpt-2-finetuning/src/train_models.sh
@@ -1,15 +1,15 @@
 #train models
 
-for batch in 1 16 32 64 128
+#for batch in 2 3 4 5
 
 
-do
-	for learn_rate in 1 0.5 0.25 0.1 0.05
+#do
+	for learn_rate in 0.001 0.0005 0.0001 0.00005 0.00001
 	
 	do 
 
-		echo "Training model with batch $batch and learning rate $learn_rate"
-		python train.py --dataset poems.npz --batch_size $batch --learning_rate $learn_rate --save_every 100 --run_name "run batch$batch lr$learn_rate"
+		echo "Training model with batch 1  and learning rate $learn_rate"
+		python train.py --dataset poems.npz --learning_rate $learn_rate --save_every 100 --run_name "run batch$batch lr$learn_rate"
 	
 	done
 
